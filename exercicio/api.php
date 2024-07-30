c957fa80b8867b6456102fe7199139174697eb09
Anderson Vanin <53703505+ProfAndersonVanin@users.noreply.github.com>
2024-07-30 08:21:13 -0300

    Criação do arquivo api.php

---
+++ exercicio/api.php
@@ -0,0 +1,21 @@
+<?php
+    header('Content-Type:application/json');
+    include 'conexao.php';
+
+    $metodo = $_SERVER['REQUEST_METHOD'];
+    //echo json_encode($metodo);
+
+    $url = $_SERVER['REQUEST_URI'];
+
+    //echo json_encode($url);
+
+    $path = parse_url($url, PHP_URL_PATH);
+    $path = trim($path,'/');
+
+    //echo json_encode($path);
+
+    $pathparts = explode('/',$path);
+
+    echo json_encode($pathparts[1]);
+
+?>
